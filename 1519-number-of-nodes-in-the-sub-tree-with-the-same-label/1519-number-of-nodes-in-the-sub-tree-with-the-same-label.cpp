436336d9d31ed5305b042e78a851ac6d0a4100a3
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-01-12 08:42:36 +0530

    Time: 636 ms (79.26%), Space: 198.8 MB (74.07%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,38 @@
+int hsh[(int)1e5+1][26];
+void dfs(int v,vector<int> &vis,unordered_map<int,vector<int>> &adj,string &s)
+{
+    vis[v]=1;
+    hsh[v][s[v]-'a']++;
+    for(auto child:adj[v])
+    {
+        if(vis[child]==0)
+        {
+            dfs(child,vis,adj,s);
+            for(int i=0;i<26;i++)
+            {
+                hsh[v][i]+=hsh[child][i];
+            }
+        }
+    }
+}
+
+class Solution {
+public:
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string s) {
+        unordered_map<int,vector<int>> adj;
+        vector<int> vis(n+1);
+        for(auto t:edges)
+        {
+            adj[t[0]].push_back(t[1]);
+            adj[t[1]].push_back(t[0]);
+        }
+        memset(hsh,0,sizeof(hsh));
+        dfs(0,vis,adj,s);
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(hsh[i][s[i]-'a']);
+        }
+        return ans;
+    }
+};
