d0c6ad86a990c7e48d3c7962bfdb12407a82ea00
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-02-04 19:03:29 +0530

    Time: 3 ms (97.53%), Space: 7.3 MB (45.48%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,47 @@
+bool check(vector<int> &a,vector<int> &b)
+{
+    for(int i=0;i<26;i++)
+    {
+        if(a[i]!=b[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+class Solution {
+public:
+    bool checkInclusion(string s, string r) {
+        int n=s.length(),m=r.length();
+        if(n>m)
+        {
+            return false;
+        }
+        vector<int> hsh1(26);
+        vector<int> hsh2(26);
+        for(auto t:s)
+        {
+            hsh1[t-'a']++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            hsh2[r[i]-'a']++;
+        }
+        if(check(hsh1,hsh2))
+        {
+            return true;
+        }
+        int i=0;
+        for(int j=n;j<m;j++)
+        {
+            hsh2[r[j]-'a']++;
+            hsh2[r[i++]-'a']--;
+            if(check(hsh1,hsh2))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
