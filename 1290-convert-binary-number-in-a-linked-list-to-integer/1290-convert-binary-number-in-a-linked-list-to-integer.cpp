23cdfec32a2e13439e6daf7c18e6d0ea456d13ef
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-02-10 09:49:15 +0530

    Time: 3 ms (53.26%), Space: 8.3 MB (69.97%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -1,43 +1,21 @@
-ListNode* rev(ListNode* v)
-{
-    if(v==NULL||v->next==NULL)
-    {
-        return v;
-    }
-    ListNode* temp=rev(v->next);
-    v->next->next=v;
-    v->next=NULL;
-    return temp;
-}
-
-ListNode* head;
-
-void reverse(ListNode* curr,ListNode* prev)
-{
-    if(curr==NULL)
-    {
-        head=prev;
-        return;
-    }
-    reverse(curr->next,curr);
-    curr->next=prev;
-}
-
-
-
 class Solution {
 public:
-    int getDecimalValue(ListNode* x) {
-        head=x;
+    int getDecimalValue(ListNode* head) {
         ListNode* prev=NULL;
-        reverse(x,prev);
-        ListNode* v=head;
-        int ans=0,cc=0;
-        while(v!=NULL)
+        ListNode* curr=head;
+        while(curr!=NULL)
         {
-            // cout<<v->val<<" ";
-            ans+=pow(2,cc)*v->val;
-            v=v->next;
+            ListNode* fwd=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=fwd;
+        }
+        int ans=0,cc=0;
+        while(prev!=NULL)
+        {
+            // cout<<prev->val<<" ";
+            ans+=pow(2,cc)*prev->val;
+            prev=prev->next;
             cc++;
         }
         // cout<<endl;
