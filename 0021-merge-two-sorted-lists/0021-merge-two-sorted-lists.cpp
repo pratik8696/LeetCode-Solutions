2a3093524541decca2e74501c3e2d3560f8583b5
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-02-10 13:18:23 +0530

    Time: 3 ms (97.43%), Space: 14.8 MB (48.55%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,57 @@
+typedef ListNode N;
+
+N* merge(N* a,N* b)
+{
+    N* curr1=a;
+    N* curr2=b;
+    N* next1=a->next;
+    N* next2=NULL;
+    while(curr1!=NULL&&curr2!=NULL)
+    {
+        int p=curr1->val,q=curr2->val,r=next1->val;
+        if(p<=q&&q<=r)
+        {
+            // curr1->curr2->next1
+            curr1->next=curr2;
+            next2=curr2->next;
+            curr2->next=next1;
+            curr1=curr2;
+            curr2=next2;
+        }
+        else
+        {
+            curr1=next1;
+            if(curr1->next==NULL)
+            {
+                curr1->next=curr2;
+                break;
+            }
+            next1=next1->next;
+        }
+    }
+    return a;
+}
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        if(a==NULL)
+        {
+            return b;
+        }
+        if(b==NULL)
+        {
+            return a;
+        }
+        if(a->val>b->val)
+        {
+            swap(a,b);
+        }
+        if(a->next==NULL)
+        {
+            a->next=b;
+            return a;
+        }
+        return merge(a,b);
+    }
+};
