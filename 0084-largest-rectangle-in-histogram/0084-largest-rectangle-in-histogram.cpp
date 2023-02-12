d7ff21e869cede6488c778a76bc86ba71fa449f5
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-02-12 21:20:56 +0530

    Time: 1148 ms (5.06%), Space: 81 MB (56.36%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,73 @@
+vector<int> right(vector<int> &arr)
+{
+    int n=arr.size();
+    vector<int> v,ans(n);
+    for(int i=n-1;i>=0;i--)
+    {
+        while(v.size()&&arr[v.back()]>=arr[i])
+        {
+            v.pop_back();
+        }
+        if(v.size()==0)
+        {
+            ans[i]=n;
+        }
+        else
+        {
+            ans[i]=v.back();
+        }
+        v.push_back(i);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return ans;
+}
+
+vector<int> left(vector<int> &arr)
+{
+    int n=arr.size();
+    vector<int> v,ans(n);
+    for(int i=0;i<n;i++)
+    {
+        while(v.size()&&arr[v.back()]>=arr[i])
+        {
+            v.pop_back();
+        }
+        if(v.size()==0)
+        {
+            ans[i]=-1;
+        }
+        else
+        {
+            ans[i]=v.back();
+        }
+        v.push_back(i);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return ans;
+}
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& arr)
+    {
+        int n=arr.size();
+        vector<int> r=right(arr);
+        vector<int> l=left(arr);
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int diff=abs(r[i]-l[i]);
+            diff--;
+            ans=max(ans,diff*arr[i]);
+        }
+        return ans;
+    }
+};
