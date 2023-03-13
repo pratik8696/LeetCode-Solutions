7fe2544edf0fdcd3f8d5bf03cb9b2beba4bbb0ca
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-03-13 12:39:40 +0530

    Time: 370 ms (10.57%), Space: 89.9 MB (7.93%) - LeetHub

---
+++ 0321-create-maximum-number/0321-create-maximum-number.cpp
@@ -0,0 +1,126 @@
+    vector<int> r(vector<int> &s, int k) {
+        deque<int> st;
+        int len=s.size()-k;
+        for(auto t:s)
+        {
+            while(st.size()&&st.back()<t&&k)
+            {
+                k--;
+                st.pop_back();
+            }
+            st.push_back(t);
+        }
+        while(k&&st.size())
+        {
+            st.pop_back();
+            k--;
+        }
+        vector<int> ans;
+        for(int i=0;i<min(len,(int)st.size());i++)
+        {
+            ans.push_back(st[i]);
+        }
+        if(ans.size()==0)
+        {
+            return {};
+        }
+        return ans;
+    }
+
+vector<int> merge(vector<int> &a,vector<int> &b)
+{
+    vector<int> res;
+    int i=0,j=0;
+    int n=a.size(),m=b.size();
+    while(i<n&&j<m)
+    {
+        if(a[i]>b[j])
+        {
+            res.push_back(a[i]);
+            i++;
+        }
+        else if(a[i]<b[j])
+        {
+            res.push_back(b[j]);
+            j++;
+        }
+        else if(a[i]==b[j])
+        {
+            int i1=i,j1=j;
+            while(i1<n&&j1<m&&a[i1]==b[j1])
+            {
+                i1++,j1++;
+            }
+            if(i1==a.size())
+            {
+                res.push_back(b[j]);
+                j++;
+            }
+            else if(j1==b.size())
+            {
+                res.push_back(a[i]);
+                i++;
+            }
+            else if(a[i1]>b[j1])
+            {
+                res.push_back(a[i]);
+                i++;
+            }
+            else if(a[i1]<b[j1])
+            {
+                res.push_back(b[j]);
+                j++;
+            }
+        }
+    }
+    while(i<n)
+    {
+        res.push_back(a[i]);
+        i++;
+    }
+    while(j<m)
+    {
+        res.push_back(b[j]);
+        j++;
+    }
+    return res;
+}
+
+class Solution {
+public:
+    vector<int> maxNumber(vector<int>& a, vector<int>& b, int k) {
+        int n=a.size(),m=b.size();
+        vector<int> ans(k,0);
+        for(int i=0;i<=k;i++)
+        {
+            // selecting only i then removing (a.size()-i)
+            vector<int> f=r(a,a.size()-i);
+            vector<int> s=r(b,b.size()-(k-i));
+            // for(auto t:f)
+            // {
+            //     cout<<t<<" ";
+            // }
+            // cout<<endl;
+            // for(auto t:s)
+            // {
+            //     cout<<t<<" ";
+            // }
+            // cout<<endl;
+            vector<int> v=merge(f,s);
+            // for(auto t:v)
+            // {
+            //     cout<<t<<" ";
+            // }
+            // cout<<endl;
+            // cout<<endl;
+            // cout<<endl;
+            // ans=larger(ans,v);
+            if(v.size()==k)
+                if(ans<v)
+                {
+                    ans=v;
+                }
+        }
+        return ans;
+    }
+};
