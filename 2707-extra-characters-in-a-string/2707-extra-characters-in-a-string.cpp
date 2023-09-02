a3688414b8c1e79cd3ddf805edf0f065e8a53c46
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-09-02 14:25:33 +0530

    Time: 671 ms (10.69%), Space: 195.9 MB (15.16%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,4 +1,5 @@
 int dp[55];
+
 int sum(int idx,string &s,map<string,int> &m)
 {
     if(idx==s.length())
@@ -9,10 +10,8 @@ int sum(int idx,string &s,map<string,int> &m)
     {
         return dp[idx];
     }
-    // leave it
     int ans=1e9;
     ans=min(ans,sum(idx+1,s,m)+1);
-    // else lete jao
     string curr="";
     for(int i=idx;i<s.length();i++)
     {
@@ -29,10 +28,7 @@ class Solution {
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
         map<string,int> m;
-        for(auto t:dictionary)
-        {
-            m[t]++;
-        }
+        for(auto t:dictionary) m[t]++;
         memset(dp,-1,sizeof(dp));
         return sum(0,s,m);
     }
