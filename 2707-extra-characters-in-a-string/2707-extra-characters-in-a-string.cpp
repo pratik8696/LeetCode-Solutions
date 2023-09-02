52ad41324184889234c10c3da43998c073f561ff
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-09-02 10:12:52 +0530

    Time: 703 ms (9.82%), Space: 195.8 MB (15.32%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,39 @@
+int dp[55];
+int sum(int idx,string &s,map<string,int> &m)
+{
+    if(idx==s.length())
+    {
+        return 0;
+    }
+    if(dp[idx]!=-1)
+    {
+        return dp[idx];
+    }
+    // leave it
+    int ans=1e9;
+    ans=min(ans,sum(idx+1,s,m)+1);
+    // else lete jao
+    string curr="";
+    for(int i=idx;i<s.length();i++)
+    {
+        curr.push_back(s[i]);
+        if(m[curr]>0)
+        {
+            ans=min(ans,sum(i+1,s,m));
+        }
+    }
+    return dp[idx] = ans;
+}
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        map<string,int> m;
+        for(auto t:dictionary)
+        {
+            m[t]++;
+        }
+        memset(dp,-1,sizeof(dp));
+        return sum(0,s,m);
+    }
+};
