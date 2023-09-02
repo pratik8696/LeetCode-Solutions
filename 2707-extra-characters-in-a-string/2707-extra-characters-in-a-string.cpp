3c573a313968e08f9f7fb381583308ff9d7e83c4
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-09-02 14:26:34 +0530

    Time: 79 ms (94.42%), Space: 62.7 MB (89.55%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,6 +1,6 @@
 int dp[55];
 
-int sum(int idx,string &s,map<string,int> &m)
+int sum(int idx,string &s,unordered_set<string> &m)
 {
     if(idx==s.length())
     {
@@ -16,7 +16,7 @@ int sum(int idx,string &s,map<string,int> &m)
     for(int i=idx;i<s.length();i++)
     {
         curr.push_back(s[i]);
-        if(m[curr]>0)
+        if(m.count(curr)>0)
         {
             ans=min(ans,sum(i+1,s,m));
         }
@@ -27,8 +27,8 @@ int sum(int idx,string &s,map<string,int> &m)
 class Solution {
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
-        map<string,int> m;
-        for(auto t:dictionary) m[t]++;
+        unordered_set<string> m;
+        for(auto t:dictionary) m.insert(t);
         memset(dp,-1,sizeof(dp));
         return sum(0,s,m);
     }
