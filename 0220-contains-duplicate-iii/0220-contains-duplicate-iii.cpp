c2bb488b5cf1059f9e3cb38bc393319fe6805831
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-09-05 18:44:40 +0530

    Time: 151 ms (76.07%), Space: 79.1 MB (22.76%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -0,0 +1,82 @@
+typedef long long ll;
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        ll n=nums.size();
+        multiset<ll> s;
+        ll diff=1e18;
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            // cout<<"BEFORE REMOVE"<<endl;
+            // for(auto t:s)
+            // {
+            //     cout<<t<<" ";
+            // }
+            // cout<<endl;
+            if(s.size()>=indexDiff+1)
+            {
+                auto it1=s.find(nums[j]);
+                ll front=1e17;
+                ll back=-1e17;
+                if(it1!=s.begin())
+                {
+                    it1--;
+                    front=*it1;
+                    it1++;
+                }
+                it1++;
+                if(it1!=s.end())
+                {
+                    back=*it1;
+                }
+                diff=min(abs(front-back),diff);
+                s.erase(s.find(nums[j]));
+                j++;
+            }
+            // cout<<"AFTER REMOVE"<<endl;
+            // for(auto t:s)
+            // {
+            //     cout<<t<<" ";
+            // }
+            // cout<<endl;
+            auto it=s.lower_bound(nums[i]);
+            if(s.size()==0)
+            {
+                s.insert(nums[i]);
+            }
+            else
+            {
+                ll front=1e17;
+                ll back=-1e17;
+                auto it=s.lower_bound(nums[i]);
+                if(it!=s.end())
+                {
+                    back=*it;
+                }
+                if(it!=s.begin())
+                {
+                    it--;
+                    front=*it;
+                }
+                diff=min(diff,abs(front-nums[i]));
+                diff=min(diff,abs(back-nums[i]));
+                s.insert(nums[i]);
+            }
+            
+            // cout<<"AFTER INSERT"<<endl;
+            // for(auto t:s)
+            // {
+            //     cout<<t<<" ";
+            // }
+            // cout<<endl;
+            // cout<<"CURRENT DIFF IS "<<diff<<endl;
+            if(diff<=valueDiff)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
