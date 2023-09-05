0c31c4a5038e2d9cedfd1cd6a090ae52dccd9b3f
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-09-05 19:10:22 +0530

    Time: 18 ms (99.13%), Space: 14.3 MB (93.66%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -0,0 +1,46 @@
+int dp[1001];
+int sum(int idx,int d,vector<int> &arr)
+{
+    int ans=0;
+    auto &x=dp[idx];
+    if(x!=-1)
+    {
+        return x;
+    }
+    // x age
+    for(int i=idx+1;i<=min(idx+d,int(arr.size()-1));i++)
+    {
+        if(arr[idx]<=arr[i])
+        {
+            break;
+        }
+        ans=max(sum(i,d,arr)+1,ans);
+    }
+    // x piche
+    for(int i=idx-1;i>=max(0,idx-d);i--)
+    {
+        if(arr[idx]<=arr[i])
+        {
+            break;
+        }
+        ans=max(sum(i,d,arr)+1,ans);
+    }
+    // conditions for jump
+    // arr[i] > arr[j]
+    // arr[i] > arr[k] for all indices between i and k
+    return x=ans;
+}
+class Solution {
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        int ans=0;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<arr.size();i++)
+        {
+            int curr=sum(i,d,arr)+1;
+            ans=max(ans,curr);
+            // cout<<curr<<" ";
+        }
+        return ans;
+    }
+};
