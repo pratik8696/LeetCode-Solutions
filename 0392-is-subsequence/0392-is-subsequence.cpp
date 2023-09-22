56814e5e67a37d540bed8cdacdbed5c0d76b3416
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-09-22 07:57:55 +0530

    Time: 3 ms (49.10%), Space: 6.8 MB (12.75%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,14 +4,8 @@ public:
         int i=0,j=0;
         while(i<s.length()&&j<t.length())
         {
-            if(s[i]==t[j])
-            {
-                i++,j++;
-            }
-            else
-            {
-                j++;
-            }
+            if(s[i]==t[j]) i++;
+            j++;
         }
         return (i==s.length());
     }
