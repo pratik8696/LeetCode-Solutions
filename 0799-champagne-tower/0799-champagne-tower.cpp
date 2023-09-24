ec38b702de9d690066d6a326c8e2295581c91d7d
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-09-25 00:44:48 +0530

    Time: 62 ms (6.15%), Space: 46.9 MB (5.53%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -8,23 +8,13 @@ public:
         {
             for(int j=1;j<=i;j++)
             {
-                // if it is greater than 1 then spread it to its children
                 double spread=max(double(dp[i][j]-1),double(0));
                 double rem=min(dp[i][j],double(1));
-                // now it should spread to its children
                 dp[i][j]=rem;
                 dp[i+1][j]+=spread/2;
                 dp[i+1][j+1]+=spread/2;
             }
         }
-        // for(int i=1;i<=10;i++)
-        // {
-        //     for(int j=1;j<=i;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         return dp[x+1][y+1];
     }
 };
