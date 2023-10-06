e93cd2478a9cbf8bbcd7b6759c02e170632040de
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-10-06 08:28:13 +0530

    Time: 3 ms (42.75%), Space: 9.5 MB (12.01%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    void dfs(string v,map<string,int> &vis,map<string,vector<pair<string,double>>> &adj,map<string,double> &dist)
+    {
+        vis[v]=1;
+        for(auto t:adj[v])
+        {
+            auto child=t.first;
+            auto wt=t.second;
+            if(vis[child]==0)
+            {
+                dist[child]*=wt*dist[v];
+                dfs(child,vis,adj,dist);
+            }
+        }
+    }
+    
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        map<string,vector<pair<string,double>>> adj;
+        set<string> s;
+        for(int i=0;i<equations.size();i++)
+        {
+            string a=equations[i][0];
+            string b=equations[i][1];
+            double val=values[i];
+            adj[a].push_back({b,val});
+            adj[b].push_back({a,1/val});
+            s.insert(a);
+            s.insert(b);
+        }
+        vector<string> total(begin(s),end(s));        
+        vector<double> ans;
+        for(auto t:queries)
+        {
+            string a=t[0];
+            string b=t[1];
+            if(s.count(a)&&s.count(b))
+            {
+                // continue
+            }
+            else
+            {
+                ans.push_back(-1);
+                continue;
+            }
+            map<string,double> dist;
+            map<string,int> vis;
+            for(auto t:total)
+            {
+                dist[t]=1;
+            }
+            dfs(a,vis,adj,dist);
+            // for(auto t:dist)
+            // {
+            //     cout<<t.first<<" "<<t.second<<endl;
+            // }
+            // cout<<endl;
+            if(vis[b]==0)
+            {
+                // cout<<-1<<endl;
+                ans.push_back(-1);
+            }
+            else
+            {
+                // cout<<dist[b]<<endl;
+                ans.push_back(dist[b]);
+            }
+        }
+        return ans;
+    }
+};
