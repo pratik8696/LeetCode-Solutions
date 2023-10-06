50b08d6daf4020efc0fb02dba8167b58c3a220ff
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-10-06 14:47:28 +0530

    Time: 228 ms (96.47%), Space: 212.6 MB (5.01%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -1,198 +1,32 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
-#pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h> 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <complex>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <list>
-#include <chrono>
-#include <random>
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <iomanip>
-#include <fstream>
- 
-using namespace std;
-using namespace __gnu_pbds;
-typedef long long ll;
-// use less_equal to make it multiset
-typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-typedef unsigned long long ull; 
-typedef long double ld;
-typedef pair<int,int> p32;
-typedef pair<ll,ll> p64;
-typedef pair<double,double> pdd;
-typedef vector<ll> v64;
-typedef vector<int> v32;
-typedef vector<vector<int> > vv32;
-typedef vector<vector<ll> > vv64;
-typedef vector<vector<p64> > vvp64;
-typedef vector<p64> vp64;
-typedef vector<p32> vp32;
-typedef vector<pair<p64, ll>> vpp64;
-typedef set<ll> s64;
-typedef set<p64> sp64;
-typedef multiset<ll> ms64;
-typedef multiset<p64> msp64;
-typedef map<ll,ll> m64;
-typedef map<ll,v64> mv64;
-typedef unordered_map<ll,v64> uv64;
-typedef unordered_map<ll,ll> u64;
-typedef unordered_map<p64, ll> up64;
-typedef unordered_map<ll, vp64> uvp64;
-typedef priority_queue<ll> pq64;
-typedef priority_queue<ll,v64,greater<ll>> pqs64;
-const int MOD = 1000000007;
-double eps = 1e-12;
-#define forn(i,n) for(ll i = 0; i < n; i++)
-#define forsn(i,s,e) for(ll i = s; i < e; i++)
-#define rforn(i,s) for(ll i = s; i >= 0; i--)
-#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(p64 x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x.first + FIXED_RANDOM) ^ splitmix64(x.second + FIXED_RANDOM);
-    }
-    size_t operator()(ll x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-typedef gp_hash_table<ll, ll, custom_hash> fm64;
-typedef gp_hash_table<p64, ll, custom_hash> fmp64;
-
-#define ln "\n"
-#define mp make_pair
-#define ie insert
-#define pb push_back
-#define fi first
-#define se second
-#define INF 2e18
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define all(x) (x).begin(), (x).end()
-#define al(arr,n) arr,arr+n
-#define sz(x) ((ll)(x).size())
-#define dbg(a) cout<<a<<endl;
-#define dbg2(a) cout<<a<<' '
-#define sizer 50001
-
-ll parent[int(5e4+1)];
-ll sparse[int(5e4+1)][30];
-ll dist[int(5e4+1)];
-ll vis[int(5e4+1)];
-uv64 adj;
-
-void bfs()
-{
-    queue<ll> q;
-    ll src=0;
-    q.push(src);
-    for(int i=0;i<sizer;i++)
-    {
-        dist[i]=0;
-        parent[i]=0;
-        vis[i]=0;
-    }
-    memset(sparse,0,sizeof(sparse));
-    vis[src]=1;
-    dist[src]=0;
-    parent[src]=src;
-    while(q.size())
-    {
-        ll curr=q.front();
-        q.pop();
-        for(auto child:adj[curr])
-        {
-            if(vis[child]==0)
-            {
-                q.push(child);
-                dist[child]=dist[curr]+1;
-                vis[child]=1;
-                parent[child]=curr;
-            }
-        }
-    }
-}
-
 class TreeAncestor {
 public:
+    int sparse[50001][30];
     TreeAncestor(int n, vector<int>& parent) {
-        adj.clear();
-        for(int i=1;i<n;i++)
+        memset(sparse,-1,sizeof(sparse));
+        for(int i=0;i<n;i++)
         {
-            int u=parent[i];
-            adj[u].pb(i);
-            adj[i].pb(u);
+            sparse[i][0]=parent[i];
         }
-        bfs();
-        forsn(i, 0, n)
+        for(int i=1;i<30;i++)
         {
-            sparse[i][0] = parent[i];
-        }
-        
-        for (ll j = 1; j < 30; j++)
-        {
-            for (ll i = 0; i < n; i++)
+            for(int j=0;j<n;j++)
             {
-                ll prev_parent=sparse[i][j-1];
-                if(prev_parent!=-1)
+                if(sparse[j][i-1]!=-1)
                 {
-                    sparse[i][j] = sparse[sparse[i][j - 1]][j - 1];    
-                }
-                else
-                {
-                    sparse[i][j]=-1;
+                    sparse[j][i]=sparse[sparse[j][i-1]][i-1];
                 }
             }
         }
-        
-//         for (ll i = 0; i < n; i++)
-//         {
-//             for (ll j = 0; j < 5; j++)
-//             {
-//                 cout<<sparse[i][j]<<" ";
-//             }
-//             cout<<endl;
-//         }
-        
-        
     }
     
     int getKthAncestor(int node, int k) {
-        ll a=node;
-        ll steps = k;
-        while (steps)
+        for(int i=0;i<30;i++)
         {
-            // a needs to come up
-            a = sparse[a][__lg(steps)];
-            steps -= powl(2, __lg(steps));
-            if(a==-1)
-            {
-                break;
-            }
+            int val = k & (1 << i);
+            if(val && node!=-1)
+                node=sparse[node][i];
         }
-        return a;
+        return node;
     }
 };
 
