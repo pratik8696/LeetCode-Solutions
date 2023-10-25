6d48050dbae78afd09e73d152462cab0be788ab5
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-10-25 18:35:22 +0530

    Time: 3 ms (51.37%), Space: 6.2 MB (53.37%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -3,38 +3,12 @@ public:
     int ans=0;
     void f(int n,int index,int reverse)
     {
-        if(n==1)
-        {
-            ans=reverse;
-            return;
-        }
-        int mid=n/2;
-        if(index>mid)
-        {
-            f(n/2,index-(n/2),(reverse+1)%2);
-        }
-        else
-        {
-            f(n/2,index,reverse);
-        }
+        if(n==1){ans=reverse;return;}
+        (index>n/2?f(n/2,index-(n/2),(reverse+1)%2):f(n/2,index,reverse));
     }
     
     int kthGrammar(int n, int k) {
         f(1<<(n-1),k,0);
         return ans;
-        /* 
-            1-0
-            2-01
-            3-0110
-            4-01101001
-            5-0110100110010110
-            16 letters hai usme se 3rd btao
-            n ho gya 4 and length is now 8
-            n ho gya 3 and length is now 4 (position is now 2)
-            n ho gya 2 and length is now 2 (reverse)
-            n ho gya 1 and length is now 1 (reverse)
-            1st row mai 1st symbol
-            2nd row mai 1st symbol
-        */
     }
 };
