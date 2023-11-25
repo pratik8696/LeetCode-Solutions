c896447ffe3dddaad8572debab0b622ca6ff59c4
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2023-11-25 12:56:51 +0530

    Time: 3 ms (72.92%), Space: 11.5 MB (41.15%) - LeetHub

---
+++ 0969-pancake-sorting/0969-pancake-sorting.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> pancakeSort(vector<int>& arr) {
+        vector<int> ans;
+        int j=arr.size()-1,cc=1,i=0;
+        while(j >= 0)
+        {
+            while(j>=0 && arr[j] == cc){j--,cc++;}
+            if(j<0){break;}
+            for(i=0;i<j;i++){if(arr[i]==cc){break;}}
+            reverse(begin(arr),begin(arr)+i+1);
+            reverse(begin(arr),begin(arr)+j+1);
+            ans.push_back(i+1);
+            ans.push_back(j+1);
+        }
+        ans.push_back(arr.size());
+        return ans;
+    }
+};
