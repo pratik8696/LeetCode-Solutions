f0c0016a30185acc4efc073c17c5be28d5ae1c07
Pratik Anand <85107447+pratik8696@users.noreply.github.com>
2024-01-04 20:38:36 +0530

    Time: 83 ms (60.07%), Space: 34.2 MB (80.82%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,199 +1,52 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
-#pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <complex>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <list>
-#include <chrono>
-#include <random>
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <iomanip>
-#include <fstream>
-
-using namespace std;
-using namespace __gnu_pbds;
-typedef long long ll;
-// use less_equal to make it multiset
-typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-typedef unsigned long long ull;
-typedef long double ld;
-typedef pair<int, int> p32;
-typedef pair<ll, ll> p64;
-typedef pair<double, double> pdd;
-typedef vector<ll> v64;
-typedef vector<int> v32;
-typedef vector<vector<int>> vv32;
-typedef vector<vector<ll>> vv64;
-typedef vector<vector<p64>> vvp64;
-typedef vector<p64> vp64;
-typedef vector<p32> vp32;
-typedef vector<pair<p64, ll>> vpp64;
-typedef set<ll> s64;
-typedef set<p64> sp64;
-typedef multiset<ll> ms64;
-typedef multiset<p64> msp64;
-typedef map<ll, ll> m64;
-typedef map<ll, v64> mv64;
-typedef unordered_map<ll, v64> uv64;
-typedef unordered_map<ll, ll> u64;
-typedef unordered_map<p64, ll> up64;
-typedef unordered_map<ll, vp64> uvp64;
-typedef priority_queue<ll> pq64;
-typedef priority_queue<ll, v64, greater<ll>> pqs64;
-const int MOD = 1000000007;
-double eps = 1e-12;
-#define forn(i, n) for (ll i = 0; i < n; i++)
-#define forsn(i, s, e) for (ll i = s; i < e; i++)
-#define rforn(i, s) for (ll i = s; i >= 0; i--)
-#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(p64 x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x.first + FIXED_RANDOM) ^ splitmix64(x.second + FIXED_RANDOM);
-    }
-    size_t operator()(ll x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-typedef gp_hash_table<ll, ll, custom_hash> fm64;
-typedef gp_hash_table<p64, ll, custom_hash> fmp64;
-
-#define ln "\n"
-#define mp make_pair
-#define ie insert
-#define pb push_back
-#define fi first
-#define se second
-#define INF 2e18
-#define fast_cin()                    \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define all(x) (x).begin(), (x).end()
-#define al(arr, n) arr, arr + n
-#define sz(x) ((ll)(x).size())
-#define dbg(a) cout << a << endl;
-#define dbg2(a) cout << a << ' ';
-using ld = long double;
-using db = double;
-using str = string; // yay python!
-// INPUT
-#define tcT template <class T
-#define tcTU tcT, class U
-#define tcTUU tcT, class... U
-tcT > void re(T &x)
-{
-    cin >> x;
-}
-tcTUU > void re(T &t, U &...u)
-{
-    re(t);
-    re(u...);
-}
-
-struct DSU
-{
-    v64 e, sz;
-    DSU(ll n)
-    {
-        e.assign(n + 1, -1);
-        sz.assign(n + 1, 1);
-    }
-    bool same(ll a, ll b) { return find(a) == find(b); }
-    ll size(ll x) { return sz[find(x)]; }
-    ll find(ll x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
-    void join(ll a, ll b)
-    {
-        a = find(a);
-        b = find(b);
-        if (a != b)
-        {
-            if (sz[a] < sz[b])
-            {
-                swap(a, b);
-            }
-            e[b] = a;
-            sz[a] += sz[b];
-        }
-    }
-};
-
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+    vector<int> par;
+    int find(int v){
+        if(par[v]==v)return v;
+        return par[v]=find(par[v]);
+    }
+    void uni(int x,int y){
+        x=find(x),y=find(y);
+        if(x!=y){
+            par[max(x,y)]=min(x,y);
+        }
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& g) {
+        int n = g.size();
+        par.clear();
+        for(int i=0;i<n+1;i++)par.push_back(i);
         map<string,int> m;
-        map<int,string> dm;
-        map<int,int> parents;
-        int cc=0;
-        for(int j=0;j<accounts.size();j++)
-        {
-            vector<string> s=accounts[j];
-            for(int i=1;i<s.size();i++)
-            {
-                if(m.count(s[i])==0)
-                {
-                    cc++;
-                    m[s[i]]=cc;
-                    dm[cc]=s[i];
+
+        for(int i=0;i<n;i++){
+            for(int j=1;j<g[i].size();j++){
+                if(m.find(g[i][j]) == m.end()){
+                    m[g[i][j]]=i;
                 }
-                parents[m[s[i]]] = j;
+                else{
+                    uni(i,m[g[i][j]]);
+                }
             }
         }
-        DSU d(cc+1);
-        for(int j=0;j<accounts.size();j++)
-        {
-            vector<string> s=accounts[j];
-            for(int i=1;i<s.size()-1;i++)
-            {
-                int curr1=m[s[i]];
-                int curr2=m[s[i+1]];
-                d.join(curr1,curr2);
+
+        vector<string> merge_mail[n];
+        vector<vector<string>> ans;
+
+        for(auto t:m){
+            int node=t.second;
+            string email=t.first;
+            merge_mail[find(node)].push_back(email);
+        }
+
+        for(int i=0;i<n;i++){
+            if(merge_mail[i].size()==0)continue;
+            vector<string> curr;
+            curr.push_back(g[i][0]);
+            sort(merge_mail[i].begin(),merge_mail[i].end());
+            for(auto t : merge_mail[i]){
+                curr.push_back(t);
             }
+            ans.push_back(curr);
         }
-        map<int,vector<int>> ans;
-        for(int i=1;i<=cc;i++)
-        {
-            ans[d.find(i)].push_back(i);
-        }
-        vector<vector<string>> res;
-        for(auto t:ans)
-        {
-            vector<string> current;
-            string res1=accounts[parents[t.first]][0];
-            for(auto x:t.second)
-            {
-                current.push_back(dm[x]);
-            }
-            sort(begin(current),end(current));
-            reverse(begin(current),end(current));
-            current.push_back(res1);
-            reverse(begin(current),end(current));
-            res.push_back(current);
-        }
-        return res;
+        return ans;
     }
 };
